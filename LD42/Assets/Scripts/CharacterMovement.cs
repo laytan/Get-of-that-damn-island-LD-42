4c8bb6e45c618629190d516ea2ff31538a33558f
laytan <Laytanlaats@hotmail.com>
2018-08-11 02:08:22 +0200

    Backup 1

---
+++ LD42/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+
+    Vector3 posToMove;
+    bool canMove = false;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Left();
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Right();
+        }
+        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Up();
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Down();
+        }
+
+        if(canMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, posToMove, speed * Time.deltaTime);
+            if(transform.position == posToMove)
+            {
+                canMove = false;
+            }
+        }
+    }
+
+    void Right()
+    {
+        if(transform.rotation.eulerAngles.z != 270)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
+        else
+        {
+            if (!canMove)
+            {
+                posToMove = new Vector3(transform.position.x + 1, transform.position.y);
+                canMove = true;
+            }
+        }
+    }
+    void Left()
+    {
+        if (transform.rotation.eulerAngles.z != 90)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        else
+        {
+            if (!canMove)
+            {
+                posToMove = new Vector3(transform.position.x - 1, transform.position.y);
+                canMove = true;
+            }
+        }
+    }
+    void Up()
+    {
+        if (transform.rotation.eulerAngles.z != 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            if (!canMove)
+            {
+                posToMove = new Vector3(transform.position.x, transform.position.y + 1);
+                canMove = true;
+            }
+        }
+    }
+    void Down()
+    {
+        if (transform.rotation.eulerAngles.z != 180)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        else
+        {
+            if (!canMove)
+            {
+                posToMove = new Vector3(transform.position.x, transform.position.y - 1);
+                canMove = true;
+            }
+        }
+    }
+}
---
+++ LD42/Assets/Scripts/Tiles.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiles : MonoBehaviour {
+
+    private List<GameObject> tileOrderedOnDistance = new List<GameObject>();
+    public float tileDestroyDelay;
+
+    // Use this for initialization
+    void Start () {
+        //Start of game initialize the ordered array on distance
+        CreateTileIndex();
+        StartCoroutine("DestroyRandomTile");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Populates tileOrderedOnDistance list
+    //TODO: There is probably a better way of doing this!
+    void CreateTileIndex()
+    {
+        //Get all the tiles, these are in a random order
+        GameObject[] randomOrder = GameObject.FindGameObjectsWithTag("Tile");
+
+        //Have 2 lists that puts the tile in one and distance in the other but on the same index so we can sort
+        List<GameObject> tiles = new List<GameObject>();
+        List<float> distances = new List<float>();
+
+        //Loop through all the tiles and add the tile and distances to their lists
+        foreach (GameObject tile in randomOrder)
+        {
+            float distance = Vector3.Distance(tile.transform.position, Vector3.zero);
+            tiles.Add(tile);
+            distances.Add(distance);
+        }
+
+        //Get the amount of tiles before we begin removing from the list
+        int tileAmount = tiles.Count;
+
+        //Loop through all tiles, get the max distance that is in the tiles and add it to our final list,
+        //then remove the tile and distance from their list (Until no more values are there and all is sorted)
+        for(int i = 0; i < tileAmount; i++)
+        {
+            float maxDistance = Mathf.Max(distances.ToArray());
+            int index = distances.IndexOf(maxDistance);
+
+            tileOrderedOnDistance.Add(tiles[index]);
+
+            tiles.RemoveAt(index);
+            distances.RemoveAt(index);
+        }
+    }
+
+    //Waits for the tileDestroyDelay and destroy's the tile that's furthest away from the middle
+    IEnumerator DestroyRandomTile()
+    {
+        if (tileOrderedOnDistance.Count > 1)
+        {
+            yield return new WaitForSeconds(tileDestroyDelay);
+
+            GameObject target = tileOrderedOnDistance[0];
+            Destroy(target);
+            tileOrderedOnDistance.Remove(target);
+
+            StartCoroutine("DestroyRandomTile");
+        }
+        else
+        {
+            Debug.Log("No more tiles left");
+            //TODO: We won?
+        }
+    }
+}
